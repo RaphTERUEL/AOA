97a9f4cb1f17fd78a3260a98013140dd9673b532
Raphaël TERUEL <raphael.teruel@isty.uvsq.fr>
2020-03-17 15:32:22 +0100

    sujet51

---
+++ sujet5.c
@@ -0,0 +1,153 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "rdtsc.h"
+
+void initVect(float** t);
+void initMat(float*** m, unsigned long n);
+void baseline (unsigned n , float ** m, float * x , float * y);
+void l1 (unsigned n, float** m, float * x, float * y);
+void l2 (unsigned n, float** m, float * x, float * y);
+void l3 (unsigned n, float** m, float * x, float * y);
+void ram (unsigned n, float** m, float * x, float * y);
+
+//pour voir caracteristique de votre ordinateur, tapez sur le terminal "lscpu"
+
+int main(int nbarg, char *argv[])
+{
+	//Listing.1 – Code de réduction d’un tableau
+	unsigned long long cb,ca;
+  unsigned long n = atol(argv[1]);
+  float * x=(float*)malloc(n*sizeof(float));
+  float * y=NULL;
+  float ** m=NULL;
+
+  //printf("%ld\n",sizeof(float)); //sizeof(float) = 4
+  //Initialisation des tableaux
+  initVect(&y);
+  initMat(&m, n);
+
+  // Premier relevé de temps
+  cb = rdtsc();
+  //appel baseline
+	#if BASELINE
+  	baseline(n, m, x, y);
+	#endif
+	#if L1
+		l1(n, m, x, y);
+	#endif
+	#if L2
+		l2(n, m, x, y);
+	#endif
+	#if L3
+		l3(n, m, x, y);
+	#endif
+	#if RAM
+		ram(n, m, x, y);
+	#endif
+  //Deuxième relevé de temps
+  ca = rdtsc();
+  printf("%lld\n",(ca-cb));
+	return 0;
+}
+
+// Initialisation du tableau
+void initVect(float** t)
+{
+  unsigned long i;
+
+  //Allocation du vecteur
+  (*t) = (float *) malloc ( sizeof(float) * 16);
+
+  //Affectation
+  for (i = 0; i < 16; i++)
+  {
+    (*t)[i] = 1;
+  }
+
+  return;
+}
+
+void initMat(float*** m, unsigned long n)
+{
+  unsigned i, j;
+
+  //Allocation de la matrice
+  (*m) = (float **) malloc( sizeof(float *) * n );
+  for (i = 0; i < n; i++)
+    (*m)[i] = (float *) malloc (sizeof(float) * n);
+
+  //Affectation
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
+      (*m)[i][j] = 1;
+    }
+  }
+
+  return;
+}
+
+
+
+//Fonction à coder
+void baseline (unsigned n, float** m, float * x, float * y)
+{
+  unsigned i , j , k ;
+  for ( i = 0; i < n ; i ++)
+    x [i] = 0;
+
+  for ( k = 0; k < 16; k ++)
+    for ( j = 0; j < n ; j ++)
+      for ( i = 0; i < n ; i ++)
+        x [i] += m [i][j] * y [k];
+}
+
+void l1 (unsigned n, float** m, float * x, float * y)
+{
+  unsigned i , j , k ;
+  for ( i = 0; i < n ; i ++)
+    x [i] = 0;
+
+  for ( k = 0; k < 16; k ++)
+    for ( j = 0; j < n ; j ++)
+      for ( i = 0; i < n ; i ++)
+        x [i] += m [i][j] * y [k];
+}
+
+void l2 (unsigned n, float** m, float * x, float * y)
+{
+  unsigned i , j , k ;
+  for ( i = 0; i < n ; i ++)
+    x [i] = 0;
+
+  for ( k = 0; k < 16; k ++)
+    for ( j = 0; j < n ; j ++)
+      for ( i = 0; i < n ; i ++)
+        x [i] += m [i][j] * y [k];
+}
+
+void l3 (unsigned n, float** m, float * x, float * y)
+{
+  unsigned i , j , k ;
+  for ( i = 0; i < n ; i ++)
+    x [i] = 0;
+
+  for ( k = 0; k < 16; k ++)
+    for ( j = 0; j < n ; j ++)
+      for ( i = 0; i < n ; i ++)
+        x [i] += m [i][j] * y [k];
+}
+
+void ram (unsigned n, float** m, float * x, float * y)
+{
+  unsigned i , j , k ;
+  for ( i = 0; i < n ; i ++)
+    x [i] = 0;
+
+  for ( k = 0; k < 16; k ++)
+    for ( j = 0; j < n ; j ++)
+      for ( i = 0; i < n ; i ++)
+        x [i] += m [i][j] * y [k];
+}
